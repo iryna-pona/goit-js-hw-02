9e23f3e713fccd5ec6bd795744fd681c141d8dd2
pona <ipona3710@gmail.com>
2025-07-15 11:27:39 +0300

    update task-2 error

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,9 @@
 function formatMessage(message, maxLength) {
-    if (message.Length <= maxLength) {
-        return message.slice();
-    } else {
-        return `${message.slice(0, maxLength)}...`;
-    }
+  if (message.Length <= maxLength) {
+    return `"${message.slice()}"`;
+  } else {
+    return `"${message.slice(0, maxLength)}..."`;
+  }
 }
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
